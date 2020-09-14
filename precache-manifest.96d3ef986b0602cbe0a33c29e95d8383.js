baa41d2c65de23d9d95350ccadbfe88bed8ee25c
Sahana Srinivasan <sahanas009@gmail.com>
2020-09-14 10:49:45 -0700

    Updates

---
+++ precache-manifest.96d3ef986b0602cbe0a33c29e95d8383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9e4ee673c9af1505c148dc1f6a8fff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "216d7dea43f2bc76bb9d",
+    "url": "/static/css/main.a7f58161.chunk.css"
+  },
+  {
+    "revision": "6f6ff4f91685c950b1cc",
+    "url": "/static/js/2.c514b22f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c514b22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216d7dea43f2bc76bb9d",
+    "url": "/static/js/main.76b4ebf0.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  }
+]);
